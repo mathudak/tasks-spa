3b31c19335351fae44c0e942b810deef3b549a98
Matej Hudak <mat.hudak@gmail.com>
2023-02-13 10:26:01 +0100

    Adds basic routes file

---
+++ src/app/routes/routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'tasks', pathMatch: 'full'},
+  { path: 'tasks', component: TasksComponent, pathMatch: 'full'}
+]
