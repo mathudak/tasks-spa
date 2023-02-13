4c369d4c9d2d3005d74dd38a2781684bd21e47cb
Matej Hudak <mat.hudak@gmail.com>
2023-02-13 10:26:19 +0100

    Adds basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
+import { RoutesModule } from './routes/routes.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { LayoutModule } from './layout/layout.module';
     BrowserModule,
     LayoutModule,
     CoreModule,
+    RoutesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/routes/routes.module.ts
+++ src/app/routes/routes.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { RouterModule } from '@angular/router';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskEditComponent } from './task-edit/task-edit.component';
+import { routes } from './routes';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TasksComponent,
+    TaskEditComponent
+  ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class RoutesModule { }
