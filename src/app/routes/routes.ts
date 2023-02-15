eee8f26c9059bc287a992aa1058d85c38fb88e41
Matej Hudak <mat.hudak@gmail.com>
2023-02-15 14:32:26 +0100

    Adds routing for task edit Minor table fix

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
+import { TaskEditComponent } from './task-edit/task-edit.component';
 import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'tasks', pathMatch: 'full'},
-  { path: 'tasks', component: TasksComponent, pathMatch: 'full'}
+  { path: 'tasks', component: TasksComponent, pathMatch: 'full'},
+  { path: 'task/:id', component: TaskEditComponent, pathMatch: 'full'},
+  { path: 'task', component: TaskEditComponent, pathMatch: 'full'}
 ]
